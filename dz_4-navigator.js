bcec11d9bf5eb6437c758274cde035f85f657e8d
Tatjana <metida.tat@gmail.com>
2024-02-24 15:36:25 +0300

    dz_4-navigator

---
+++ dz_4-navigator.js
@@ -0,0 +1,11 @@
+//Написать код, рассчитывающий дистанцию объекта.
+//Посчитать расстояние от текущего до назначенного местоположения.
+//d = √ (х2 – х1)2 + (у2 – у1)2
+const addressLatx = 120; //адрес назначения по x2
+const addressLongy = 80; //адрес назначения по y2
+
+const positionLatx = 20; //текущее по x1
+const positionLongy = 10; //текущее по y1
+
+let deltaDifference = Math.round(Math.sqrt((Number(addressLatx - positionLatx) + Number(addressLongy - positionLongy))**2))
+console.log(deltaDifference);
