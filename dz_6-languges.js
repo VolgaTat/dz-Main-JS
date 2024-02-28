6768c4963fd8253f7d160b7254e68b63f1c9ecfa
Tatjana <metida.tat@gmail.com>
2024-02-28 16:06:38 +0300

    Булевая логика

---
+++ dz_6-languges.js
@@ -0,0 +1,26 @@
+//Написать код в одну строчку, которая проверит 
+//возможность вечти машину по следующим параметрам:
+
+let hasLicence = true;// имеет права
+let age = 30;         // возрост
+let isDrunk = false;  // пьян
+
+console.log((age >= 18 && hasLicence && !isDrunk) ? "Можно водить машину" : "Нельзя водить! Только игрушечную)")
+
+hasLicence = true;
+age = 10;
+isDrunk = false;
+
+console.log((age >= 18 && hasLicence && !isDrunk) ? "Можно водить машину" : "Нельзя водить! Только игрушечную)")
+
+hasLicence = false;
+age = 10;
+isDrunk = false;
+
+console.log((age >= 18 && hasLicence && !isDrunk) ? "Можно водить машину" : "Нельзя водить! Только игрушечную)")
+
+hasLicence = false;
+age = 22;
+isDrunk = true;
+
+console.log((age >= 18 && hasLicence && !isDrunk) ? "Можно водить машину" : "Нельзя водить! Только игрушечную)")
